844048ad372d4267553af44efd87128b9a0829e7
igor <igorvasiliev7"gmail.com>
2018-11-23 16:44:12 +0200

    Initial commit

---
+++ src/main/java/ua/com/markovka/MyServerSpringApplication.java
@@ -0,0 +1,11 @@
+package ua.com.markovka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServerSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyServerSpringApplication.class, args);
+    }
+}
