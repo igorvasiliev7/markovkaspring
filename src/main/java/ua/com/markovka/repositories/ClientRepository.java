2613897875cc60943d2fc5f0043a1f7e5de2a435
igor <igorvasiliev7"gmail.com>
2018-11-27 17:58:37 +0200

    JpaRepositories added2

--- src/main/java/ua/com/markovka/repositories/ClientRepository.java
+++ src/main/java/ua/com/markovka/repositories/ClientRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.repository.query.Param;
 import ua.com.markovka.model.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long>{
+
+    Client findByPhone(String phone);
 }
 
 //    @Query("select new myserver.dto.OrderDTO(o.id, u.id, u.name,u.email,p.name,p.os,p.price,o.name, o.address, o.date) from Order o, User u, Phone p, UserOrder uo where u.id = :id and uo.userId = u.id and uo.orderId = o.id and o.phoneId = p.id")
